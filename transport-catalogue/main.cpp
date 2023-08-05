33178e9077effa4bb4d474df5391aed7c57805b3
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-05 11:03:02 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,7 +1,7 @@
 #include "input_reader.h"
 #include "stat_reader.h"
 
-// main.cpp — точка входа.
+
 int main() {
     infocatalogueclass::TransportCatalogue catalogue;
     fill::TransportCatalogue(catalogue);
