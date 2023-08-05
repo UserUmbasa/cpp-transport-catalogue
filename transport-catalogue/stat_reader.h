67879937893552ea52dcab315e6c63b15345b071
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-05 11:00:30 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,9 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+#include <iostream>
+
+namespace out {
+	void Transport(infocatalogueclass::TransportCatalogue& catalogue);
+	void Bus(infocatalogueclass::TransportCatalogue&, std::string);
+	void Stop(infocatalogueclass::TransportCatalogue&, std::string);
+}
