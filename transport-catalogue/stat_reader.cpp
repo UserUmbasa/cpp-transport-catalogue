0dca43b7283b4f3b6d391f5ff904b7f804a7e8f5
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-10 19:12:16 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -2,7 +2,7 @@
 
 #include <iomanip>
 
-void out::Transport(std::istream& input, info_catalogue_class::TransportCatalogue& catalogue) {
+void out::Show_data_on_request(std::istream& input, info_catalogue_class::TransportCatalogue& catalogue, std::ostream& out) {
     std::string count;
     std::getline(input, count);
     auto index = stoi(count);
@@ -11,49 +11,49 @@ void out::Transport(std::istream& input, info_catalogue_class::TransportCatalogu
         std::cin >> keyword;
         std::getline(std::cin, inf);
         if (keyword == "Bus") {
-            out::Bus(catalogue, inf);
+            out::Show_Bus(catalogue, inf, out);
         }
         if (keyword == "Stop") {
-            out::Stop(catalogue, inf);
+            out::Show_Stop(catalogue, inf, out);
         }
     }
 }
 
-void out::Bus(info_catalogue_class::TransportCatalogue& ctlg, std::string inf) 
+void out::Show_Bus(info_catalogue_class::TransportCatalogue& ctlg, std::string inf, std::ostream& out)
 {
     std::string number = inf.substr(1, inf.npos); 
     if (ctlg.FindBusRoute(number))    
     {
-        auto Chek = ctlg.BusRouteInformation(number);
-        std::cout << "Bus " << number << ": " 
-            << Chek.stops_count << " stops on route, "
-            << Chek.unique_stops_count << " unique stops, "
-            << Chek.route_length << " route length, "
-            << Chek.curvature << " curvature\n";
+        auto check = ctlg.BusRouteInformation(number);
+            out << "Bus " << number << ": " 
+            << check.stops_count << " stops on route, "
+            << check.unique_stops_count << " unique stops, "
+            << check.route_length << " route length, "
+            << check.curvature << " curvature\n";
     }
     else {
-        std::cout << "Bus " << number << ": not found\n";
+           out << "Bus " << number << ": not found\n";
     }
 }
 
 
-void out::Stop(info_catalogue_class::TransportCatalogue& ctlg, std::string inf) {
+void out::Show_Stop(info_catalogue_class::TransportCatalogue& ctlg, std::string inf, std::ostream& out) {
     std::string stop_name = inf.substr(1, inf.npos);
     if (ctlg.FindBusStop(stop_name)) {
-        std::cout << "Stop " << stop_name << ": ";
+        out << "Stop " << stop_name << ": ";
         std::set<std::string> buses = ctlg.BusToStop(stop_name);
         if (!buses.empty()) {
-            std::cout << "buses ";
+            out << "buses ";
             for (const auto& bus : buses) {
-                std::cout << bus << " ";
+                out << bus << " ";
             }
-            std::cout << "\n";
+            out << "\n";
         }
         else {
-            std::cout << "no buses\n";
+            out << "no buses\n";
         }
     }
     else {
-        std::cout << "Stop " << stop_name << ": not found\n";
+        out << "Stop " << stop_name << ": not found\n";
     }
 }
