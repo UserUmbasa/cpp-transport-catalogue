18d6b5d3aa96e0fb71cde1638cb29fa33a94ca27
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-10 19:09:54 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -8,9 +8,10 @@
 
 namespace fill {
 
-	void TransportCatalogue(std::istream& input, info_catalogue_class::TransportCatalogue&);
-	info_struct::Bus ParseBus(std::string&);
-	info_struct::Stop ParseStop(std::string&);
-	void AddStopDistances(std::string&, info_catalogue_class::TransportCatalogue&);
+	void TransportCatalogue(std::istream& input, info_catalogue_class::TransportCatalogue& ctlg);
+	info_struct::Bus ParseBus(std::string& str);
+	info_struct::Stop ParseStop(std::string& str);
+	void AddStopDistances(std::string&, info_catalogue_class::TransportCatalogue& ctlg);
 }
+
 
