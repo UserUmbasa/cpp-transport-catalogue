47dc4a8e0c2c3231ca326e00d411798968f9ff2a
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-10 19:11:13 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,9 +1,8 @@
 #include "input_reader.h"
 #include "stat_reader.h"
 
-
 int main() {
     info_catalogue_class::TransportCatalogue catalogue;
     fill::TransportCatalogue(std::cin, catalogue);
-    out::Transport(std::cin, catalogue);
+    out::Show_data_on_request(std::cin, catalogue, std::cout);
 }
