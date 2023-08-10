5e311e81db03a4a6f7b34402171fdd02a869dd96
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-10 11:00:38 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,33 +1,10 @@
-namespace info_catalogue_class
+#pragma once
+#include "transport_catalogue.h"
+#include <iostream>
+/*stat_reader.h, stat_reader.cpp — чтение запросов на вывод и сам вывод;*/
+namespace out 
 {
-	namespace out
-	{
-		namespace bus
-		{
-			struct Bus_Query_Result 
-			{
-				std::string_view name;
-				bool not_found;
-				size_t stops_on_route;
-				size_t unique_stops;
-				size_t route_length;
-				double curvature;
-			};
-			Bus_Query_Result query(TransportCatalogue& catalogue, std::string_view str);
-			void Show(Bus_Query_Result bus_info, std::ostream& output);
-		}
-		namespace stop
-		{
-			struct Stop_Query_Result 
-			{
-				std::string_view name;
-				bool not_found;
-				std::vector <std::string_view> buses_name;
-			};
-			Stop_Query_Result query(TransportCatalogue& catalogue, std::string_view stop_name);
-			void Show(Stop_Query_Result bus_info, std::ostream& output);			
-		}
-		void Transport(std::istream& input, info_catalogue_class::TransportCatalogue& ctlg);
-		void Split_Search(info_catalogue_class::TransportCatalogue& ctlg, std::string_view str);
-	}
+	void Transport(std::istream& input, info_catalogue_class::TransportCatalogue& catalogue);
+	void Bus(info_catalogue_class::TransportCatalogue&, std::string);
+	void Stop(info_catalogue_class::TransportCatalogue&, std::string);
 }
