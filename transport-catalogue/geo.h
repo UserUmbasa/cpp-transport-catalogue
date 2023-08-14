56238025c9687d76ebb570d65804f8e263c6acff
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-14 12:11:20 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <cmath>
-namespace coordinate {
+const int RADIUSEARTH = 6371000;
+namespace geo {
+
     struct Coordinates {
         double lat;
         double lng;
@@ -13,14 +14,6 @@ namespace coordinate {
         }
     };
 
-    inline double ComputeDistance(Coordinates from, Coordinates to) {
-        using namespace std;
-        if (from == to) {
-            return 0;
-        }
-        static const double dr = 3.1415926535 / 180.;
-        return acos(sin(from.lat * dr) * sin(to.lat * dr)
-            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-            * 6371000;
-    }
+    double ComputeDistance(Coordinates from, Coordinates to);
+
 }
