d9332d6bda4e0c192586e9da5432430e7ce9fdcd
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-14 15:13:53 +0300

    Create json_reader.h

---
+++ transport-catalogue/json_reader.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "json.h"
+#include "transport_catalogue.h"
+#include "map_renderer.h"
+#include "request_handler.h"
+
+#include <iostream>
+
+class jReader {
+public:
+    jReader(std::istream& input) : input_(json::Load(input)) {
+    }
+
+    const json::Node& RenderSettings() const;
+    const json::Node& BaseRequests() const;
+    const json::Node& StatRequests() const;
+    void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;
+    void FillCatalogue(info_catalogue::TransportCatalogue& ctlg);
+    renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
+    const json::Node PrintStop(const json::Dict& request_, RequestHandler& rh) const;
+    const json::Node PrintRoute(const json::Dict& request_map, RequestHandler& rh) const;
+    const json::Node PrintMap(const json::Dict& request_, RequestHandler& rh) const;
+
+private:
+    json::Document input_;
+    json::Node null_ = nullptr;
+
+    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& request_map) const;
+    void FillStopDistances(info_catalogue::TransportCatalogue& ctlg) const;
+    std::tuple<std::string_view, std::vector<const domain::Stop*>, bool> FillRoute(const json::Dict& request_map, info_catalogue::TransportCatalogue& ctlg) const;
+};
