404dc69c61ad80114f97b39532e1e68ae5fa88ce
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-27 08:05:17 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,18 +1,22 @@
 #include "json_reader.h"
 #include "request_handler.h"
 
-int main() {
+int main() 
+{
+
     info_catalogue::TransportCatalogue catalogue;
     jReader json_doc(std::cin);
 
     json_doc.FillCatalogue(catalogue);
 
     const auto& stat_requests = json_doc.StatRequests();
+
     const auto& render_settings = json_doc.RenderSettings().AsMap();
+
     const auto& renderer = json_doc.FillRenderSettings(render_settings);
 
     RequestHandler rh(catalogue, renderer);
-    json_doc.ProcessRequests(stat_requests, rh);
 
-  
+    rh.ProcessRequests(stat_requests);   
+        
 }
