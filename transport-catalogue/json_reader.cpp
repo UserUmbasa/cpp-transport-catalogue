083eff2322a4ac38568006f33f559ee5b2d5db7c
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-08-27 08:03:20 +0300

    Update json_reader.cpp

--- transport-catalogue/json_reader.cpp
+++ transport-catalogue/json_reader.cpp
@@ -30,27 +30,6 @@ const json::Node& jReader::RenderSettings() const
     return (tmp->second);
 }
 
-void jReader::ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const {
-    json::Array result;
-    for (auto& request : stat_requests.AsArray()) {
-        const auto& request_map = request.AsMap();
-        const auto& type = request_map.at("type").AsString();
-        if (type == "Stop") {
-            result.push_back(PrintStop(request_map, rh).AsMap());
-        }
-
-        if (type == "Bus") {
-            result.push_back(PrintRoute(request_map, rh).AsMap());
-        }
-
-        if (type == "Map") {
-            result.push_back(PrintMap(request_map, rh).AsMap());
-        }
-
-    }
-    json::Print(json::Document{ result }, std::cout);
-}
-
 void jReader::FillCatalogue(info_catalogue::TransportCatalogue& ctlg) {
     const json::Array& arr = BaseRequests().AsArray();
     for (auto& request_stops : arr) {
@@ -72,6 +51,7 @@ void jReader::FillCatalogue(info_catalogue::TransportCatalogue& ctlg) {
         }
     }
 }
+
 std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> jReader::FillStop(const json::Dict& request_map) const {
     std::string_view stop_name = request_map.at("name").AsString();
     geo::Coordinates coordinates = { request_map.at("latitude").AsDouble(), request_map.at("longitude").AsDouble() };
@@ -123,7 +103,7 @@ renderer::MapRenderer jReader::FillRenderSettings(const json::Dict& request_) co
     render_settings.stop_label_font_size = request_.at("stop_label_font_size").AsInt();
     const json::Array& stop_label_offset = request_.at("stop_label_offset").AsArray();
     render_settings.stop_label_offset = { stop_label_offset[0].AsDouble(), stop_label_offset[1].AsDouble() };    
-    
+
     if (request_.at("underlayer_color").IsString())  
     {
         render_settings.underlayer_color = request_.at("underlayer_color").AsString();
@@ -133,9 +113,12 @@ renderer::MapRenderer jReader::FillRenderSettings(const json::Dict& request_) co
     {
         const json::Array& underlayer_color = request_.at("underlayer_color").AsArray();
         render_settings.underlayer_color = ParseRgb(underlayer_color);
+
     }
     else throw std::logic_error("error color");
+
     render_settings.underlayer_width = request_.at("underlayer_width").AsDouble();
+
     const json::Array& color_palette = request_.at("color_palette").AsArray();
     for (const auto& color_element : color_palette) 
     {
@@ -156,57 +139,10 @@ renderer::MapRenderer jReader::FillRenderSettings(const json::Dict& request_) co
     return render_settings;
 }
 
-const json::Node jReader::PrintRoute(const json::Dict& request_map, RequestHandler& rh) const {
-    json::Dict result;
-    const std::string& route_number = request_map.at("name").AsString();
-    result["request_id"] = request_map.at("id").AsInt();
-    if (!rh.BusNumber(route_number)) {
-        result["error_message"] = json::Node{ static_cast<std::string>("not found") };
-    }
-    else {
-        result["curvature"] = rh.GetBusStat(route_number)->curvature;
-        result["route_length"] = rh.GetBusStat(route_number)->route_length;
-        result["stop_count"] = static_cast<int>(rh.GetBusStat(route_number)->stops_count);
-        result["unique_stop_count"] = static_cast<int>(rh.GetBusStat(route_number)->unique_stops_count);
-    }
 
-    return json::Node{ result };
-}
-
-
-
-const json::Node jReader::PrintStop(const json::Dict& request_, RequestHandler& rh) const {
-    json::Dict result;
-    const std::string& stop_name = request_.at("name").AsString();
-    result["request_id"] = request_.at("id").AsInt();
-    if (!rh.StopName(stop_name)) {
-        result["error_message"] = json::Node{ static_cast<std::string>("not found") };
-    }
-    else {
-        json::Array buses;
-        for (auto& bus : rh.GetBusesByStop(stop_name)) {
-            buses.push_back(bus);
-        }
-        result["buses"] = buses;
-    }
-
-    return json::Node{ result };
-}
-
-const json::Node jReader::PrintMap(const json::Dict& request_, RequestHandler& rh) const {
-    json::Dict result;
-    result["request_id"] = request_.at("id").AsInt();
-    std::ostringstream strm;
-    svg::Document map = rh.RenderMap();
-    map.Render(strm);
-    result["map"] = strm.str();
-
-    return json::Node{ result };
-}
 
 svg::Color jReader::ParseRgb(const json::Array& color) const
 {
-    svg::Color tmp; //std::variant<std::monostate, std::string,svg::Rgb, svg::Rgba>;
     if (color.size() == 3)
     {
         tmp = svg::Rgb(color[0].AsInt(), color[1].AsInt(), color[2].AsInt());
@@ -219,3 +155,4 @@ svg::Color jReader::ParseRgb(const json::Array& color) const
     }
     else throw std::logic_error("error colortype");
 }
+
