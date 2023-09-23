847ca53ab444fa590173a8e67ff5bfe475eec3a1
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-09-23 13:07:17 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -3,20 +3,19 @@
 
 int main() 
 {
-
-    info_catalogue::TransportCatalogue catalogue;
-    jReader json_doc(std::cin);
-
-    json_doc.FillCatalogue(catalogue);
-
-    const auto& stat_requests = json_doc.StatRequests();
-
-    const auto& render_settings = json_doc.RenderSettings().AsMap();
-
-    const auto& renderer = json_doc.FillRenderSettings(render_settings);
-
-    RequestHandler rh(catalogue, renderer);
-
-    rh.ProcessRequests(stat_requests);   
         
+    info_catalogue::TransportCatalogue catalogue;
+    jReader doc(std::cin);
+
+    doc.FillCatalogue(catalogue);
+
+    const auto& stat_requests = doc.StatRequests();
+    const auto& render_settings = doc.RenderSettings();
+    const auto& renderer = doc.FillRenderSettings(render_settings);
+    const auto& routing_settings = doc.FillRoutingSettings(doc.RoutingSettings());
+    const transport_router::Router router = { routing_settings, catalogue };
+
+    RequestHandler rh(catalogue, renderer, router);
+    rh.ProcessRequests(stat_requests);
+
 }
