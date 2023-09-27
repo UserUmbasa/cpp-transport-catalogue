40c1da6dfba0472e39b134a82ff0785adf3fdffe
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-09-27 13:14:22 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -21,10 +21,11 @@ namespace transport_router {
 		}
 		
 		const std::optional<graph::Router<double>::RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
+		//тут не совсем понял...он нужен в request_handler.cpp 139
 		const graph::DirectedWeightedGraph<double>& GetGraph() const;
 
 	private:
-		void Add_Stop(const info_catalogue::TransportCatalogue& catalogue);
+		void Add_Stop(const info_catalogue::TransportCatalogue& catalogue) ;
 		void Add_Bus(const info_catalogue::TransportCatalogue& catalogue);
 		int bus_wait_time_ = 0;
 		double bus_velocity_ = 0.0;
@@ -32,5 +33,6 @@ namespace transport_router {
 		graph::DirectedWeightedGraph<double> graph_;
 		std::map<std::string, graph::VertexId> stop_ids_;
 		std::unique_ptr<graph::Router<double>> router_;
+		
 	};
 }
