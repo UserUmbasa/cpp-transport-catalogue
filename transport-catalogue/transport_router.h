d160ba873586288970e21d8718ed18cebde40f54
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-09-27 12:48:25 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "router.h"
+#include"json.h"
 #include "transport_catalogue.h"
 
 #include <memory>
@@ -11,21 +12,20 @@ namespace transport_router {
 	public:
 		Router() = default;
 
-		Router(const int bus_wait_time, const double bus_velocity)
-			: bus_wait_time_(bus_wait_time)
-			, bus_velocity_(bus_velocity) {}
-
-		Router(const Router& settings, const info_catalogue::TransportCatalogue& catalogue) {
-			bus_wait_time_ = settings.bus_wait_time_;
-			bus_velocity_ = settings.bus_velocity_;
-			BuildGraph(catalogue);
+		Router(const json::Node& settings, const info_catalogue::TransportCatalogue& catalogue)
+		{
+			bus_wait_time_ = settings.AsMap().at("bus_wait_time").AsInt();
+			bus_velocity_ = settings.AsMap().at("bus_velocity").AsDouble();
+			Add_Stop(catalogue);
+			Add_Bus(catalogue);
 		}
-
-		const graph::DirectedWeightedGraph<double>& BuildGraph(const info_catalogue::TransportCatalogue& catalogue);
+		
 		const std::optional<graph::Router<double>::RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
 		const graph::DirectedWeightedGraph<double>& GetGraph() const;
 
 	private:
+		void Add_Stop(const info_catalogue::TransportCatalogue& catalogue);
+		void Add_Bus(const info_catalogue::TransportCatalogue& catalogue);
 		int bus_wait_time_ = 0;
 		double bus_velocity_ = 0.0;
 
@@ -33,5 +33,4 @@ namespace transport_router {
 		std::map<std::string, graph::VertexId> stop_ids_;
 		std::unique_ptr<graph::Router<double>> router_;
 	};
-
 }
