122c63df34742855b6c7f6d196f7be97bec0509d
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-09-29 07:42:31 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -12,24 +12,20 @@ namespace transport_router {
 	public:
 		Router() = default;
 
-		Router(const json::Node& settings, const info_catalogue::TransportCatalogue& catalogue)
+		Router(const json::Node& settings, const info_catalogue::TransportCatalogue& catalogue):
+		routset_{ settings.AsMap().at("bus_wait_time").AsInt(),settings.AsMap().at("bus_velocity").AsDouble() }		
 		{
-			bus_wait_time_ = settings.AsMap().at("bus_wait_time").AsInt();
-			bus_velocity_ = settings.AsMap().at("bus_velocity").AsDouble();
-			Add_Stop(catalogue);
-			Add_Bus(catalogue);
+			AddStop(catalogue);
+			AddBus(catalogue);
 		}
 		
 		const std::optional<graph::Router<double>::RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
-		//тут не совсем понял...он нужен в request_handler.cpp 139
-		const graph::DirectedWeightedGraph<double>& GetGraph() const;
+		const graph::Edge<double> & GetGraph(const size_t edge_id) const;
 
 	private:
-		void Add_Stop(const info_catalogue::TransportCatalogue& catalogue) ;
-		void Add_Bus(const info_catalogue::TransportCatalogue& catalogue);
-		int bus_wait_time_ = 0;
-		double bus_velocity_ = 0.0;
-
+		void AddStop(const info_catalogue::TransportCatalogue& catalogue) ;
+		void AddBus(const info_catalogue::TransportCatalogue& catalogue);
+		domain::RoutSet routset_;
 		graph::DirectedWeightedGraph<double> graph_;
 		std::map<std::string, graph::VertexId> stop_ids_;
 		std::unique_ptr<graph::Router<double>> router_;
