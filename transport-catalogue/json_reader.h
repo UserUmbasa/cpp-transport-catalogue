ecec41fa3000046bb9b0f3499c4540e4349bfa8f
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-09-29 08:00:09 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -7,6 +7,8 @@
 #include "svg.h"
 #include"json_builder.h"
 #include <iostream>
+#include <sstream>
+using Catalogue = info_catalogue::TransportCatalogue;
 
 class jReader
 {
@@ -19,10 +21,16 @@ public:
     const json::Node& StatRequests() const;
     const json::Node& RoutingSettings() const;
 
-    void FillCatalogue(info_catalogue::TransportCatalogue& ctlg);
+    void FillCatalogue(Catalogue& ctlg);
     renderer::MapRenderer FillRenderSettings(const json::Node& request_map) const;
-
     svg::Color ParseRgb(const json::Array& color) const;
+    //--------------print----------
+    void ProcessRequests(const json::Node& stat_requests, const Catalogue& ctlg) const;
+    const json::Node PrintRoute(const json::Dict& request_map, const Catalogue& ctlg) const;
+    const json::Node PrintStop(const json::Dict& request_map, const Catalogue& ctlg) const;
+    const json::Node PrintMap(const json::Dict& request_map, const Catalogue& ctlg) const;
+    const json::Node PrintRouting(const json::Dict& request_map, const Catalogue& ctlg) const;
+    
 
 private:
     json::Document input_;
@@ -30,6 +38,5 @@ private:
 
     std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& request_map) const;
     void FillStopDistances(info_catalogue::TransportCatalogue& ctlg) const;
-    std::tuple<std::string_view, std::vector<const domain::Stop*>, bool> FillRoute(const json::Dict& request_map, info_catalogue::TransportCatalogue& ctlg) const;
+    std::tuple<std::string_view, std::vector<const domain::Stop*>, bool> FillRoute(const json::Dict& request_map, Catalogue& ctlg) const;
 };
-
